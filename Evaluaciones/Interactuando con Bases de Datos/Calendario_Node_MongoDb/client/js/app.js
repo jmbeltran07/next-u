38d58bd902720efada1b7575fdba68468fed0714
jmbeltra <jmbeltra@JMBELTRA-LAP.oradev.oraclecorp.com>
2018-07-03 22:42:14 -0500

    finished project

--- Evaluaciones/Interactuando con Bases de Datos/Calendario_Node_MongoDb/client/js/app.js
+++ Evaluaciones/Interactuando con Bases de Datos/Calendario_Node_MongoDb/client/js/app.js
@@ -1,122 +1,143 @@
-
 class EventManager {
-    constructor() {
-        this.urlBase = "/events"
-        this.obtenerDataInicial()
-        this.inicializarFormulario()
-        this.guardarEvento()
-    }
+  constructor() {
+    this.urlBase = "/events"
+    this.obtenerDataInicial()
+    this.inicializarFormulario()
+    this.guardarEvento()
+  }
 
-    obtenerDataInicial() {
-        let url = this.urlBase + "/all"
-        $.get(url, (response) => {
-            this.inicializarCalendario(response)
-        })
-    }
+  obtenerDataInicial() {
+    let url = this.urlBase + "/all"
+    $.get(url, (response) => {
+      this.inicializarCalendario(response)
+    })
+  }
 
-    eliminarEvento(evento) {
-        let eventId = evento.id
-        $.post('/events/delete/'+eventId, {id: eventId}, (response) => {
-            alert(response)
-        })
-    }
+  eliminarEvento(evento) {
+    let eventId = evento.id
+    $.post('/events/delete/' + eventId, {
+      id: eventId
+    }, (response) => {
+      alert(response)
+    })
+  }
 
-    guardarEvento() {
-        $('.addButton').on('click', (ev) => {
-            ev.preventDefault()
-            let nombre = $('#titulo').val(),
-            start = $('#start_date').val(),
-            title = $('#titulo').val(),
-            end = '',
-            start_hour = '',
-            end_hour = '';
+  actualizarEvento(evento) {
+    var id = evento.id,
+      start_date = evento.start.calendar(),
+      end_date =  evento.end.calendar()
 
-            if (!$('#allDay').is(':checked')) {
-                end = $('#end_date').val()
-                start_hour = $('#start_hour').val()
-                end_hour = $('#end_hour').val()
-                start = start + 'T' + start_hour
-                end = end + 'T' + end_hour
-            }
-            let url = this.urlBase + "/new"
-            if (title != "" && start != "") {
-                let ev = {
-                    title: title,
-                    start: start,
-                    end: end
-                }
-                $.post(url, ev, (response) => {
-                    alert(response)
-                })
-                $('.calendario').fullCalendar('renderEvent', ev)
-            } else {
-                alert("Complete los campos obligatorios para el evento")
-            }
-        })
-    }
+      var form_data = new FormData()
 
-    inicializarFormulario() {
-        $('#start_date, #titulo, #end_date').val('');
-        $('#start_date, #end_date').datepicker({
-            dateFormat: "yy-mm-dd"
-        });
-        $('.timepicker').timepicker({
-            timeFormat: 'HH:mm:ss',
-            interval: 30,
-            minTime: '5',
-            maxTime: '23:59:59',
-            defaultTime: '',
-            startTime: '5:00',
-            dynamic: false,
-            dropdown: true,
-            scrollbar: true
-        });
-        $('#allDay').on('change', function(){
-            if (this.checked) {
-                $('.timepicker, #end_date').attr("disabled", "disabled")
-            }else {
-                $('.timepicker, #end_date').removeAttr("disabled")
-            }
-        })
-    }
+    form_data.append('id', id)
+    form_data.append('start_date', start_date)
+    form_data.append('end_date', end_date)
 
-    inicializarCalendario(eventos) {
-        $('.calendario').fullCalendar({
-            header: {
-                left: 'prev,next today',
-                center: 'title',
-                right: 'month,agendaWeek,basicDay'
-            },
-            defaultDate: '2016-11-01',
-            navLinks: true,
-            editable: true,
-            eventLimit: true,
-            droppable: true,
-            dragRevertDuration: 0,
-            timeFormat: 'H:mm',
-            eventDrop: (event) => {
-                this.actualizarEvento(event)
-            },
-            events: eventos,
-            eventDragStart: (event,jsEvent) => {
-                $('.delete').find('img').attr('src', "img/trash-open.png");
-                $('.delete').css('background-color', '#a70f19')
-            },
-            eventDragStop: (event,jsEvent) => {
-                var trashEl = $('.delete');
-                var ofs = trashEl.offset();
-                var x1 = ofs.left;
-                var x2 = ofs.left + trashEl.outerWidth(true);
-                var y1 = ofs.top;
-                var y2 = ofs.top + trashEl.outerHeight(true);
-                if (jsEvent.pageX >= x1 && jsEvent.pageX<= x2 &&
-                    jsEvent.pageY >= y1 && jsEvent.pageY <= y2) {
-                        this.eliminarEvento(event)
-                        $('.calendario').fullCalendar('removeEvents', event.id);
-                    }
-                }
-            })
+    $.post('/events/update/' + id, {
+      id: id,
+      start_date: start_date,
+      end_date: end_date
+    }, (response) => {
+      alert(response)
+    })
+  }
+
+  guardarEvento() {
+    $('.addButton').on('click', (ev) => {
+      ev.preventDefault()
+      let nombre = $('#titulo').val(),
+        start = $('#start_date').val(),
+        title = $('#titulo').val(),
+        end = '',
+        start_hour = '',
+        end_hour = '';
+
+      if (!$('#allDay').is(':checked')) {
+        end = $('#end_date').val()
+        start_hour = $('#start_hour').val()
+        end_hour = $('#end_hour').val()
+        start = start + 'T' + start_hour
+        end = end + 'T' + end_hour
+      }
+      let url = this.urlBase + "/new"
+      if (title != "" && start != "") {
+        let ev = {
+          title: title,
+          start: start,
+          end: end
         }
-    }
+        $.post(url, ev, (response) => {
+          alert(response)
+        })
+        $('.calendario').fullCalendar('renderEvent', ev)
+      } else {
+        alert("Complete los campos obligatorios para el evento")
+      }
+    })
+  }
 
-    const Manager = new EventManager()
+  inicializarFormulario() {
+    $('#start_date, #titulo, #end_date').val('');
+    $('#start_date, #end_date').datepicker({
+      dateFormat: "yy-mm-dd"
+    });
+    $('.timepicker').timepicker({
+      timeFormat: 'HH:mm:ss',
+      interval: 30,
+      minTime: '5',
+      maxTime: '23:59:59',
+      defaultTime: '',
+      startTime: '5:00',
+      dynamic: false,
+      dropdown: true,
+      scrollbar: true
+    });
+    $('#allDay').on('change', function() {
+      if (this.checked) {
+        $('.timepicker, #end_date').attr("disabled", "disabled")
+      } else {
+        $('.timepicker, #end_date').removeAttr("disabled")
+      }
+    })
+  }
+
+  inicializarCalendario(eventos) {
+    $('.calendario').fullCalendar({
+      header: {
+        left: 'prev,next today',
+        center: 'title',
+        right: 'month,agendaWeek,basicDay'
+      },
+      defaultDate: '2018-06-29',
+      navLinks: true,
+      editable: true,
+      eventLimit: true,
+      droppable: true,
+      dragRevertDuration: 0,
+      timeFormat: 'H:mm',
+      eventDrop: (event) => {
+        this.actualizarEvento(event)
+      },
+      events: eventos,
+      eventDragStart: (event, jsEvent) => {
+        $('.delete').find('img').attr('src', "img/trash-open.png");
+        $('.delete').css('background-color', '#a70f19')
+      },
+      eventDragStop: (event, jsEvent) => {
+        var trashEl = $('.delete');
+        var ofs = trashEl.offset();
+        var x1 = ofs.left;
+        var x2 = ofs.left + trashEl.outerWidth(true);
+        var y1 = ofs.top;
+        var y2 = ofs.top + trashEl.outerHeight(true);
+        if (jsEvent.pageX >= x1 && jsEvent.pageX <= x2 &&
+          jsEvent.pageY >= y1 && jsEvent.pageY <= y2) {
+          this.eliminarEvento(event)
+          $('.calendario').fullCalendar('removeEvents', event.id);
+        }
+      }
+    })
+  }
+}
+
+const Manager = new EventManager();
